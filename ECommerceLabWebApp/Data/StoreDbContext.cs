61b19da138179beea32b4da346096c7745ed0984
Matthew Johnson <matthew.james.johnson@hotmail.com>
2020-04-27 12:44:57 -0700

    Added db setup for cart and cart items.

--- ECommerceLabWebApp/Data/StoreDbContext.cs
+++ ECommerceLabWebApp/Data/StoreDbContext.cs
@@ -23,6 +23,9 @@ namespace ECommerceLabWebApp.Data
         /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //adding composite key for CartItems table.
+            modelBuilder.Entity<CartItems>().HasKey(x => new { x.CartID, x.ProductID });
+
             //seeding our data
             modelBuilder.Entity<Product>().HasData(
 
@@ -129,5 +132,7 @@ namespace ECommerceLabWebApp.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<CartItems> CartItems { get; set; }
     }
 }
---
+++ ECommerceLabWebApp/Models/CartItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceLabWebApp.Models
+{
+    public class CartItems
+    {
+        public int ID { get; set; }
+        public int CartID { get; set; }
+        public int ProductID { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
